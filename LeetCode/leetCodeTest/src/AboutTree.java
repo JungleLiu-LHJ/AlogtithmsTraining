0a38ea99b398a41bf75df5826b6464c8bcb66a00
20200318007 <405153053@qq.com>
2020-11-26 23:23:14 +0800

    平衡二叉树

--- LeetCode/leetCodeTest/src/AboutTree.java
+++ LeetCode/leetCodeTest/src/AboutTree.java
@@ -141,5 +141,24 @@ public class AboutTree {
         }
         return out;
     }
+
+    /**
+     * 剑指 Offer 28. 对称的二叉树
+     *请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null) return true;
+        return recur(root.left,root.right);
+    }
+
+    private boolean recur(TreeNode left,TreeNode right) {
+        if(left == null && right==null) return true;
+        if(left == null ||right ==null||left.val!=right.val)return false;
+        return recur(left.left,right.right) && recur(left.right , right.left);
+    }
+
 }
 
